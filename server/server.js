371009a2ac25eb206a69dd608ece0c6cac59a991
Miguel <miguelmesa220@gmail.com>
2026-05-04 17:56:03 -0400

    prepared the backend to store latitude and longitude from users

--- server/server.js
+++ server/server.js
@@ -4,6 +4,20 @@ const app = express();
 
 const PORT = 5000;
 
+app.get("/", (req, res) => {
+    res.send("Backend functional");
+});
+
+app.get("/gyms", (req, res) => {
+   const latitude = req.query.lat;
+   const longitude = req.query.lng;
+
+   console.log(latitude, longitude)
+
+   res.json({latitude, longitude})
+});
+
 app.listen(PORT, ()=> {
-    console.log(`server running on https://localhost:${PORT}`);
+    console.log(`server running on http://localhost:${PORT}`);
 } );
+
