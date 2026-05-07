4636f8f156531cfd6b4ce33baec27d061bee5e20
Miguel Mesa <miguelmesa220@gmail.com>
2026-05-06 21:47:22 -0400

    connected backend to Google Places

--- server/server.js
+++ server/server.js
@@ -1,21 +1,54 @@
+
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
 const PORT = 5000;
 
+
+
 app.get("/", (req, res) => {
     res.send("Backend functional");
 });
 
-app.get("/gyms", (req, res) => {
+app.get("/gyms", async (req, res) => {
    const latitude = req.query.lat;
    const longitude = req.query.lng;
 
-   console.log(latitude, longitude)
+   const url = "https://places.googleapis.com/v1/places:searchNearby";
 
-   res.json({latitude, longitude})
+   const requestBody = {
+          includedTypes: ["gym"],
+          maxResultCount: 15,
+          locationRestriction: {
+            circle: {
+                center: {
+                    latitude: Number(latitude),
+                    longitude: Number(longitude),
+                },
+                radius: 10000,
+            },
+          },
+   };
+
+   const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json", 
+            "X-Goog-Api-Key": process.env.GOOGLE_PLACES_API_KEY,
+            "X-Goog-FieldMask": "places.displayName,places.formattedAddress,places.rating",
+        },
+        body: JSON.stringify(requestBody),
+   });
+
+   const data = await response.json();
+   
+   res.json(data);
+
 });
+
 
 app.listen(PORT, ()=> {
     console.log(`server running on http://localhost:${PORT}`);
