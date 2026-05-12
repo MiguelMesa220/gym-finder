32b702a26bac1c4caa3cf921b38e70b0c0037806
Miguel Mesa <miguelmesa220@gmail.com>
2026-05-11 22:25:04 -0400

    added option to edit location coordinates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,37 +3,57 @@ import './App.css'
 
 function App() {
   const [gyms, setGyms] = useState([]);
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
 
   async function handleSearch(){
-    const response = await fetch("http://localhost:5000/gyms?lat=43&lng=-79");
+    const response = await fetch(`http://localhost:5000/gyms?lat${latitude}}&lng=${longitude}`);
     const data = await response.json();
     setGyms(data);
     console.log("button clicked");
     console.log(data);
+
+
   }
 
   
 
   return (
     <div>
-    <button onClick = {handleSearch}>
-      Find Gyms
-    </button>
+      
+      <input
+        type="text"
+        placeholder="Latitude"
+        value={latitude}
+        onChange={(lat)=> setLatitude(lat.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Longitude"
+        value={longitude}
+        onChange={(lng)=> setLongitude(lng.target.value)}
+      />
 
 
-    {gyms.map((gym) => (
-      <div key ={gym.id}>
-        <h2>
-          {gym.name}
-        </h2>
-        <p>
-          {gym.address}
-        </p>
-        <p>
-          {gym.rating} stars
-        </p>
-      </div>
-    ))}
+      <button onClick = {handleSearch}>
+        Find Gyms
+      </button>
+
+
+      {gyms.map((gym) => (
+        <div key ={gym.id}>
+          <h2>
+            {gym.name}
+          </h2>
+          <p>
+            {gym.address}
+          </p>
+          <p>
+            {gym.rating} stars
+          </p>
+        </div>
+      ))}
     </div>
 
   )
